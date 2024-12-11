d2d2b0c0fce49b028ca7c311a819e3d7ad1da360
zakyyl <zaky24112003@gmail.com>
2024-12-11 20:17:16 +0700

    🐛 fix: Fixed a code in file server

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const app = express();
 const path = require("path");
 require("dotenv").config();
 
-const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
