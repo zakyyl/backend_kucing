3ec723dc4e943c1335dde44d4492538c5d0ccc2b
zakyyl <zaky24112003@gmail.com>
2024-12-12 16:43:06 +0700

    feat(deploy):update vercel config

---
+++ src/config/config.js
@@ -0,0 +1,51 @@
+// {
+//   "development": {
+//     "username": "postgres",
+//     "password": "123",
+//     "database": "kucingbe",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres"
+//   }
+// }
+
+require("dotenv").config();
+
+const config = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+
+  },
+}
+
+module.exports = config;
